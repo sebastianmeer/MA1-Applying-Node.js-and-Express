6cb0f9cedcbffe10c44b6e426b7f4d3ead4fed59
Sebastian Meer <sebastianmeer196@gmail.com>
2026-04-05 13:27:59 +0800

    fix: instantiate Global Mongoose Connection inside Vercel Serverless Function

--- api/index.js
+++ api/index.js
@@ -1,2 +1,24 @@
 const app = require('../app');
-module.exports = app;
+const mongoose = require('mongoose');
+
+// Cached connection check
+const connectToDatabase = async () => {
+    if (mongoose.connection.readyState >= 1) {
+        return;
+    }
+    const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+    await mongoose.connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+// Vercel Serverless Function Request Handler
+module.exports = async (req, res) => {
+    try {
+        await connectToDatabase();
+        return app(req, res);
+    } catch (err) {
+        res.status(500).json({ error: 'Database connection failed' });
+    }
+};
